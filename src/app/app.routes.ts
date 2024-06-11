06204e2405ce63a5e45a4afdbe39489f3772dfd4
Jv Empleo <empleojv28@gmail.com>
2024-06-11 14:32:14 +0800

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent,
+  },
+];
